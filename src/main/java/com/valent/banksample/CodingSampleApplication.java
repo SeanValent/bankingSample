7afda3749049251c0c4126ab671ab278faa6b51e
SeanValent <svalent87@yahoo.com>
2019-06-22 01:50:47 -0400

    Update CodingSampleApplication.java

--- src/main/java/com/valent/banksample/CodingSampleApplication.java
+++ src/main/java/com/valent/banksample/CodingSampleApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CodingChallengeApplication {
+public class CodingSampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CodingChallengeApplication.class, args);
+		SpringApplication.run(CodingSampleApplication.class, args);
 	}
 }
